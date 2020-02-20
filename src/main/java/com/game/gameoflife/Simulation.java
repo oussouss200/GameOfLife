ca8a995d10fb63c2db023dcf7ad0e973007983c9
oussouss200 <jarrayoussama.issats@gmail.com>
2020-02-20 01:23:52 +0100

    Count alive neighbours commit

--- src/main/java/com/game/gameoflife/Simulation.java
+++ src/main/java/com/game/gameoflife/Simulation.java
@@ -35,20 +35,60 @@ public class Simulation {
         this.board[x][y] = 0;
     }
 
-    public int countAliveNeighbours(int x,int y){
+    public int countAliveNeighbours(int x, int y) {
         int count = 0;
-        count += this.board[x-1][y-1];
-        count += this.board[x][y-1];
-        count += this.board[x+1][y-1];
 
-        count += this.board[x-1][y];
-        count += this.board[x+1][y];
+        count += getState(x - 1, y - 1);
+        count += getState(x, y - 1);
+        count += getState(x + 1, y - 1);
 
-        count += this.board[x-1][y+1];
-        count += this.board[x][y+1];
-        count += this.board[x+1][y+1];
+        count += getState(x - 1, y);
+        count += getState(x + 1, y);
 
-        return  count;
+        count += getState(x - 1, y + 1);
+        count += getState(x, y + 1);
+        count += getState(x + 1, y + 1);
+
+        return count;
+    }
+
+    public int getState(int x, int y) {
+        if (x < 0 || x >= width) {
+            return 0;
+        }
+
+        if (y < 0 || y >= height) {
+            return 0;
+        }
+
+        return this.board[x][y];
+    }
+
+    public void step() {
+        int[][] newBoard = new int[width][height];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int aliveNeighbours = countAliveNeighbours(x, y);
+
+                if (getState(x, y) == 1) {
+                    if (aliveNeighbours < 2) {
+                        newBoard[x][y] = 0;
+                    } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                        newBoard[x][y] = 1;
+                    } else if (aliveNeighbours > 3) {
+                        newBoard[x][y] = 0;
+                    }
+                } else {
+                    if (aliveNeighbours == 3) {
+                        newBoard[x][y] = 1;
+                    }
+                }
+
+            }
+        }
+
+        this.board = newBoard;
     }
 
     public static void main(String[] args) {
@@ -59,7 +99,12 @@ public class Simulation {
         simulation.setAlive(4,2);
 
         simulation.printBoard();
+        simulation.step();
 
-        System.out.println(simulation.countAliveNeighbours(3,2));
+        simulation.printBoard();
+        simulation.step();
+
+        simulation.printBoard();
+
     }
 }
